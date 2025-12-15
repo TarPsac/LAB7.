623597ef9d889fc84fdf712073e880d993b7702b
TarPsac <TPFZoneTH@gmail.com>
2025-12-15 18:32:01 +0700

    lab7_3. Completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,9 @@
-#include<iostream>
-using namespace std;
-
-char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char x) {
+    if (x < 'A'|| x > 'Z') {
+        return '0';
+    } else if (x >= 'B' && x <= 'Z') {
+        return(x - 1);
+    } else {
+        return 'Z';
+    }
 }
